f66e08900f898eee5ee387d0eaf56bd2de96c69b
Suman <sumanksah729@gmail.com>
2023-12-10 17:29:37 +0530

    Thusis my second commit

--- astm.java
+++ astm.java
@@ -1,5 +1,5 @@
  class astm {
     public static void main(String[] args) {
-        System.out.println("Welcome to my world"); 
+        System.out.println("Welcome bosss tooo my world"); 
     }
 }
